2b4416c11b6e93b0b893cd53f0cb16d7cf2e0e28
Tom KAUFFELD <tom@kauffeld-it.fr>
2025-12-08 23:39:16 +0100

    Day 7 + 8

---
+++ AdventOfCode.Day07/Program.cs
@@ -0,0 +1,98 @@
+﻿using AdventOfCode.Core;
+using AdventOfCode.Core.Helpers;
+
+namespace AdventOfCode.Day07
+{
+    internal static class Program
+    {
+        private static async Task<Field<bool>> ReadInput(bool test = false)
+        {
+            return await InputFileHelper.GetField<bool>(7, test);
+        }
+        
+        private static async Task Part1(bool test = false)
+        {
+            Field<bool> field = await ReadInput(test);
+            Queue<(int x, int y)> todo = [];
+            for (int x = 0; x < field.Width; x++)
+                if (field.Get(x, 0) == 'S')
+                    todo.Enqueue((x, 0));
+            long total = 0;
+            while (todo.TryDequeue(out (int x, int y) position))
+            {
+                for (int y = position.y; y < field.Height; ++y)
+                {
+                    if (field.GetCustomData(position.x, y) is true)
+                        break;
+                    char c = field.Get(position.x, y);
+                    field.SetCustomData(position.x, y, true);
+                    if (c == '^')
+                    {
+                        if (position.x > 0)
+                            todo.Enqueue((position.x - 1, y));
+                        if (position.x < field.Width - 1)
+                            todo.Enqueue((position.x + 1, y));
+                        ++total;
+                        break;
+                    }
+                }
+            }
+            
+            Console.WriteLine($"Part 1: {total}");
+        }
+
+
+        private static long CountPaths(Field<bool> field, int x, int y, ref Dictionary<(int x, int y), long> cache)
+        {
+            if (cache.TryGetValue((x, y), out long count))
+                return count;
+            if (field.Get(x, y) == '^')
+            {
+                long left = x > 0 ? CountPaths(field, x - 1, y, ref cache) : 0;
+                long right =  x < field.Width - 1 ? CountPaths(field, x + 1, y, ref cache) : 0;
+                long total = left + right;
+                cache[(x, y)] = total;
+            }
+            else if (y < field.Height - 1)
+            {
+                long total = CountPaths(field, x, y + 1, ref cache);
+                cache[(x, y)] = total;
+            }
+            else
+            {
+                cache[(x, y)] = 1;
+            }
+            return cache[(x, y)];
+        }
+        
+
+        private static async Task Part2(bool test = false)
+        {
+            Field<bool> field = await ReadInput(test);
+            Queue<(int x, int y)> todo = [];
+            for (int x = 0; x < field.Width; x++)
+                if (field.Get(x, 0) == 'S')
+                    todo.Enqueue((x, 0));
+
+            long total = 0;
+            Dictionary<(int x, int y), long> cache = new();
+            while (todo.TryDequeue(out (int x, int y) position))
+            {
+                long result = CountPaths(field, position.x, position.y, ref cache);
+                if (test)
+                    Console.WriteLine($"From {position.x},{position.y}, found {result} paths");
+                total += result;
+            }
+            
+            
+            Console.WriteLine($"Part 2: {total}");
+        }
+        
+        
+        private static async Task Main(string[] args)
+        {
+            await Part1();
+            await Part2();
+        }
+    }
+}
---
+++ AdventOfCode.Day08/Program.cs
@@ -0,0 +1,148 @@
+﻿using AdventOfCode.Core;
+using AdventOfCode.Core.Helpers;
+
+namespace AdventOfCode.Day08
+{
+    internal static class Program
+    {
+        private static async Task<List<(int x, int y, int z)>> ReadInput(bool test = false)
+        {
+            return (await InputFileHelper.GetSeparatedNumbers(8, test, ','))
+                .Select(items => (x: items[0], y: items[1], z: items[2]))
+                .ToList();
+        }
+
+        private static double Distance((int x, int y, int z) a, (int x, int y, int z) b)
+        {
+            double dx = a.x - b.x;
+            double dy = a.y - b.y;
+            double dz = a.z - b.z;
+            return Math.Sqrt(dx * dx + dy * dy + dz * dz);
+        }
+        
+        private static async Task Part1(bool test = false)
+        {
+            int connectionsToMake = test ? 10 : 1000;
+            
+            List<(int x, int y, int z)> items = await ReadInput(test);
+            List<(int a, int b, double d)> links = [];
+            
+            for (int i = 0; i < items.Count - 1; ++i)
+            {
+                for (int j = i + 1; j < items.Count; ++j)
+                {
+                    links.Add((i, j, Distance(items[i], items[j])));
+                }
+            }
+            links = links
+                .OrderBy(l => l.d)
+                .ToList();
+            
+            Dictionary<int, int> indexToGroup = [];
+            Dictionary<int, List<int>> groups = [];
+
+            for (int i = 0; i < items.Count; ++i)
+            {
+                indexToGroup.Add(i, i);
+                groups.Add(i, [i]);
+            }
+            
+            for (int i = 0; i < connectionsToMake; ++i)
+            {
+                (int a, int b, double d) link = links[i];
+                
+                int groupA = indexToGroup[link.a];
+                int groupB = indexToGroup[link.b];
+                if (groupA != groupB)
+                {
+                    (int x, int y, int z) itemA = items[link.a];
+                    (int x, int y, int z) itemB = items[link.b];
+                    foreach (int index in groups[groupB])
+                    {
+                        groups[groupA].Add(index);
+                        indexToGroup[index] = groupA;
+                    }
+                    if (test)
+                        Console.WriteLine($"Linking {itemA.x},{itemA.y},{itemA.z} and {itemB.x},{itemB.y},{itemB.z}, in group: {groups[groupA].Count}");
+                    
+                    groups.Remove(groupB);
+                }
+            }
+            
+
+            long result = groups
+                .Select(p => p.Value.Count)
+                .OrderDescending()
+                .Take(3)
+                .Aggregate(1L, (a, b) => a * b);
+            
+            Console.WriteLine($"Part 1: {result}");
+        }
+        
+        
+
+        private static async Task Part2(bool test = false)
+        {
+            List<(int x, int y, int z)> items = await ReadInput(test);
+            List<(int a, int b, double d)> links = [];
+            
+            for (int i = 0; i < items.Count - 1; ++i)
+            {
+                for (int j = i + 1; j < items.Count; ++j)
+                {
+                    links.Add((i, j, Distance(items[i], items[j])));
+                }
+            }
+            links = links
+                .OrderBy(l => l.d)
+                .ToList();
+            
+            Dictionary<int, int> indexToGroup = [];
+            Dictionary<int, List<int>> groups = [];
+
+            for (int i = 0; i < items.Count; ++i)
+            {
+                indexToGroup.Add(i, i);
+                groups.Add(i, [i]);
+            }
+            int lastLinkIndex = -1;
+            for (int i = 0; i < links.Count; ++i)
+            {
+                (int a, int b, double d) link = links[i];
+                
+                int groupA = indexToGroup[link.a];
+                int groupB = indexToGroup[link.b];
+                if (groupA != groupB)
+                {
+                    (int x, int y, int z) itemA = items[link.a];
+                    (int x, int y, int z) itemB = items[link.b];
+                    foreach (int index in groups[groupB])
+                    {
+                        groups[groupA].Add(index);
+                        indexToGroup[index] = groupA;
+                    }
+                    if (test)
+                        Console.WriteLine($"Linking {itemA.x},{itemA.y},{itemA.z} and {itemB.x},{itemB.y},{itemB.z}, in group: {groups[groupA].Count}");
+                    lastLinkIndex = i;
+                    groups.Remove(groupB);
+                    if (groups.Count < 2)
+                        break;
+                }
+            }
+            (int a, int b, double d) lastConnectedLink = links[lastLinkIndex];
+            (int x, int y, int z) lastItemA = items[lastConnectedLink.a];
+            (int x, int y, int z) lastItemB = items[lastConnectedLink.b];
+            
+            long result = (long)lastItemA.x * lastItemB.x;
+            
+            Console.WriteLine($"Part 2: {result}");
+        }
+        
+        
+        private static async Task Main(string[] args)
+        {
+            await Part1();
+            await Part2();
+        }
+    }
+}
